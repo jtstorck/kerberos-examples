e53ffbb2fbca22a9d2e54ab7f1e0b8c1b2193651
Jeff Storck <jtswork@gmail.com>
2017-11-23 12:50:29 -0500

    Updated UgiRunnable task to create files rather than retrieve a path status Replaced command line parameters for task configuration with config read from json Updated README.md with new instructions for relogin and tgt expiration tests

--- hadoop/ugi-test/src/main/java/hadoop/ugitest/UgiTestMain.java
+++ hadoop/ugi-test/src/main/java/hadoop/ugitest/UgiTestMain.java
@@ -1,23 +1,41 @@
 package hadoop.ugitest;
 
-import com.google.common.base.Preconditions;
-import com.google.common.base.Strings;
-import com.google.common.collect.Lists;
-import org.apache.commons.cli.*;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.lang.reflect.Type;
+import java.security.PrivilegedExceptionAction;
+import java.util.Collection;
+import java.util.Date;
+import java.util.LinkedList;
+import java.util.UUID;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.FileStatus;
+import org.apache.hadoop.fs.FSDataOutputStream;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.security.UserGroupInformation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
-import java.io.PrintStream;
-import java.security.PrivilegedExceptionAction;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
+import com.google.common.base.MoreObjects;
+import com.google.common.base.Preconditions;
+import com.google.common.base.Strings;
+import com.google.common.collect.Lists;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+import com.google.gson.stream.JsonReader;
 
 public class UgiTestMain {
 
@@ -25,7 +43,7 @@ public class UgiTestMain {
 
     private static final String JAVA_HOME = "JAVA_HOME";
 
-    private static final String HEADER = System.lineSeparator() + "A tool for testing logins and relogins to a kerberized HDFS cluster." +
+    private static final String HEADER = System.lineSeparator() + "A tool for testing the usage of hadoop-client UserGroupInformation with a kerberized HDFS cluster." +
             System.lineSeparator() + System.lineSeparator();
     private static final String FOOTER = System.lineSeparator() + "Java home: " + System.getenv(JAVA_HOME) + System.lineSeparator();
 
@@ -33,39 +51,30 @@ public class UgiTestMain {
             .longOpt("krb-conf")
             .desc("Kerberos configuration path")
             .hasArg()
-            .argName("filename")
+            .argName("file")
+            .required()
+            .build();
+    private static final Option OPTION_HADOOP_MIN_RELOGIN_PERIOD_SECONDS = Option.builder("m")
+            .longOpt("hadoop-relogin-min-delay")
+            .desc("Hadoop minimum relogin period (in seconds), only supported with hadoop-client 2.8+")
+            .hasArgs()
             .numberOfArgs(1)
-            .required()
-            .build();
-    private static final Option OPTION_KRB_CREDENTIALS = Option.builder("c")
-            .longOpt("krb-credential")
-            .desc("Kerberos keytab, principal separated by ','")
-            .hasArgs()
-            .argName("keytab,principal")
-            .valueSeparator(',')
-            .required()
-            .build();
-    private static final Option OPTION_KRB_RELOGIN_PERIOD = Option.builder("p")
-            .longOpt("relogin-period")
-            .desc("Kerberos relogin period in seconds")
-            .hasArgs()
             .argName("seconds")
-            .valueSeparator(',')
             .build();
-    private static final Option OPTION_HADOOP_RESOURCES = Option.builder("r")
-            .longOpt("hadoop-resources")
-            .desc("*-site.xml files used by Hadoop, separated by ','")
-            .argName("paths")
+    private static final Option OPTION_TASK_CONFIG_JSON_FILE = Option.builder("c")
+            .longOpt("task-config")
+            .desc("Task configuration file (JSON)")
             .hasArgs()
-            .valueSeparator(',')
+            .numberOfArgs(1)
+            .argName("file")
+            .type(File.class)
             .required()
             .build();
-    private static final Option OPTION_HDFS_DIR = Option.builder("d")
-            .longOpt("dir")
-            .desc("HDFS directory for which status will be retrieved")
-            .argName("path")
-            .hasArg()
-            .required()
+    private static final Option OPTION_HADOOP_RESOURCE = Option.builder("r")
+            .longOpt("hadoop-resource")
+            .desc("site.xml file used by Hadoop (repeatable argument)")
+            .argName("file")
+            .numberOfArgs(1)
             .build();
     private static final Option OPTION_MIGRATOR_HELP = Option.builder("h")
             .longOpt("help")
@@ -74,6 +83,7 @@ public class UgiTestMain {
 
     private static ScheduledExecutorService scheduledExecutorService;
     private static volatile boolean running = true;
+    private static Gson gson = new GsonBuilder().create();
 
     public static void main(String[] args) throws IOException, InterruptedException {
         PrintStream output = System.out;
@@ -93,39 +103,44 @@ public class UgiTestMain {
 
                 // set up hadoop resources configuration
                 Configuration configuration = new Configuration();
-                configuration.set("hadoop.kerberos.min.seconds.before.relogin", "1");
+                final String hadoopMinReloginSeconds = commandLine.getOptionValue(OPTION_HADOOP_MIN_RELOGIN_PERIOD_SECONDS.getOpt(), "600");
+                configuration.set("hadoop.kerberos.min.seconds.before.relogin", hadoopMinReloginSeconds);
                 // fix fs impl properties, since maven-assembly-plugin causes some of the META-INF/services files to get overwritten
                 // when files of the same name exist in multiple modules
                 configuration.set("fs.hdfs.impl", org.apache.hadoop.hdfs.DistributedFileSystem.class.getName());
                 configuration.set("fs.file.impl", org.apache.hadoop.fs.LocalFileSystem.class.getName());
-                String[] hadoopResourcesOptionValues = commandLine.getOptionValues(OPTION_HADOOP_RESOURCES.getOpt());
-                Lists.newArrayList(hadoopResourcesOptionValues).forEach(s -> configuration.addResource(new Path(s)));
-
+                String[] hadoopResourceOptionValues = MoreObjects.firstNonNull(commandLine.getOptionValues(OPTION_HADOOP_RESOURCE.getOpt()), new String[]{});
+                if (hadoopResourceOptionValues.length == 0) {
+                    LOGGER.warn("No Hadoop resources were specified");
+                }
+                Lists.newArrayList(hadoopResourceOptionValues).forEach(s -> configuration.addResource(new Path(s)));
                 UserGroupInformation.setConfiguration(configuration);
 
-                LOGGER.info("Test using kerberos config [{}], hadoop config [{}]", kerberosConfig, configuration);
+                LOGGER.info("Using kerberos config [{}], hadoop config [{}], hadoop kerberos min seconds before relogin [{}]",
+                        kerberosConfig, configuration, hadoopMinReloginSeconds);
 
                 // set up kerberos credentials and tasks
-                final int reloginAttemptPeriod = commandLine.hasOption(OPTION_KRB_RELOGIN_PERIOD.getOpt()) ? Integer.parseInt(commandLine.getOptionValue(OPTION_KRB_RELOGIN_PERIOD.getOpt())) :
-                        60;
-                final String hdfsDir = commandLine.getOptionValue(OPTION_HDFS_DIR.getOpt());
-                Preconditions.checkArgument(!Strings.isNullOrEmpty(hdfsDir), "No HDFS path was provided");
-                final String[] krbCredentialOptionValues = commandLine.getOptionValues(OPTION_KRB_CREDENTIALS.getOpt());
-                if (krbCredentialOptionValues.length % 2 != 0) {
-                    throw new IllegalArgumentException(String.format("command-line parameter %s requires one or more keytab and principal pairs separated by a ',', unable to parse properly",
-                            OPTION_KRB_CREDENTIALS.getLongOpt()));
-                }
-                scheduledExecutorService = Executors.newScheduledThreadPool(krbCredentialOptionValues.length / 2);
-                for (int i = 0; i < krbCredentialOptionValues.length; i = i + 2) {
-                    final String keytab = krbCredentialOptionValues[i];
+                final File taskConfigFile = (File) commandLine.getParsedOptionValue(OPTION_TASK_CONFIG_JSON_FILE.getOpt());
+                Preconditions.checkArgument(taskConfigFile.exists(), "Task configuration file [%s] not found", taskConfigFile);
+                Type taskConfigCollectionType = new TypeToken<LinkedList<TaskConfig>>() {
+                }.getType();
+                final Collection<TaskConfig> taskConfigs = gson.fromJson(new JsonReader(new FileReader(taskConfigFile)), taskConfigCollectionType);
+                Preconditions.checkState(taskConfigs != null, "No tasks were parsed from [%s]", taskConfigFile);
+                LOGGER.info("Tasks to be created: {}", taskConfigs);
+                scheduledExecutorService = Executors.newScheduledThreadPool(taskConfigs.size());
+                for (TaskConfig taskConfig : taskConfigs) {
+                    final String keytab = taskConfig.getKeytabPath();
                     Preconditions.checkArgument(!Strings.isNullOrEmpty(keytab), "No keytab was provided");
-                    final String principal = krbCredentialOptionValues[i + 1];
+                    final String principal = taskConfig.getPrincipal();
                     Preconditions.checkArgument(!Strings.isNullOrEmpty(principal), "No principal was provided");
-                    LOGGER.info("Attempting new login for principal [{}] from keytab [{}]", principal, keytab);
-                    UserGroupInformation ugi = UserGroupInformation.loginUserFromKeytabAndReturnUGI(principal, keytab);
-                    LOGGER.info("UGI acquired for principal [{}] from keytab [{}], UGI [{}]", principal, keytab, ugi);
-                    LOGGER.info("Scheduling relogin for principal [{}] from keytab [{}], UGI [{}], relogin period [{} second(s)]", principal, keytab, ugi, reloginAttemptPeriod);
-                    scheduledExecutorService.scheduleAtFixedRate(new UgiRunnable(ugi, configuration, hdfsDir), 60, reloginAttemptPeriod, TimeUnit.SECONDS);
+                    LOGGER.debug("Attempting new login for principal [{}] from keytab [{}]", principal, keytab);
+                    final UserGroupInformation ugi = UserGroupInformation.loginUserFromKeytabAndReturnUGI(principal, keytab);
+                    final long firstLoginTimstampSeconds = TimeUnit.MILLISECONDS.toSeconds(System.currentTimeMillis());
+                    LOGGER.debug("Acquired UGI for principal [{}] from keytab [{}], UGI [{}]", principal, keytab, ugi);
+                    scheduledExecutorService.scheduleAtFixedRate(new UgiRunnable(ugi, configuration, taskConfig, firstLoginTimstampSeconds),
+                            taskConfig.getInitialTaskDelay(), taskConfig.getTaskPeriod(), TimeUnit.SECONDS);
+                    LOGGER.debug("Scheduled task for principal [{}] from keytab [{}], UGI [{}], initial task execution delay [{} second(s)], task execution period [{} second(s)], relogin period [{} second(s)]",
+                            principal, keytab, ugi, taskConfig.getInitialTaskDelay(), taskConfig.getTaskPeriod(), taskConfig.getReloginPeriod());
                 }
             }
             while (!scheduledExecutorService.isTerminated()) {
@@ -145,10 +160,9 @@ public class UgiTestMain {
         final Options options = new Options();
         options.addOption(OPTION_MIGRATOR_HELP);
         options.addOption(OPTION_KRB_CONF_FILE);
-        options.addOption(OPTION_KRB_RELOGIN_PERIOD);
-        options.addOption(OPTION_HADOOP_RESOURCES);
-        options.addOption(OPTION_KRB_CREDENTIALS);
-        options.addOption(OPTION_HDFS_DIR);
+        options.addOption(OPTION_HADOOP_MIN_RELOGIN_PERIOD_SECONDS);
+        options.addOption(OPTION_HADOOP_RESOURCE);
+        options.addOption(OPTION_TASK_CONFIG_JSON_FILE);
         return options;
     }
 
@@ -168,27 +182,40 @@ public class UgiTestMain {
         private final Logger LOGGER;
         private final UserGroupInformation ugi;
         private final Configuration configuration;
-        private final String hdfsDir;
+        private final TaskConfig taskConfig;
+        private long lastLoginTimestampSeconds;
 
-        UgiRunnable(UserGroupInformation ugi, Configuration configuration, String hdfsDir) {
+        UgiRunnable(UserGroupInformation ugi, Configuration configuration, TaskConfig taskConfig, long firstLoginTimestampSeconds) {
+            lastLoginTimestampSeconds = firstLoginTimestampSeconds;
             this.ugi = ugi;
             this.configuration = configuration;
+            this.taskConfig = taskConfig;
             LOGGER = LoggerFactory.getLogger(UgiRunnable.class.getCanonicalName() + "." + ugi.getUserName());
-            this.hdfsDir = hdfsDir;
         }
 
         @Override
         public void run() {
             try {
-                LOGGER.info("Attempting relogin for [{}]", ugi);
+                if ((TimeUnit.MILLISECONDS.toSeconds(System.currentTimeMillis()) - lastLoginTimestampSeconds) > taskConfig.getReloginPeriod()) {
+                    LOGGER.debug("Attempting relogin for [{}]", ugi);
+                    ugi.doAs((PrivilegedExceptionAction<Void>) () -> {
+                        ugi.reloginFromKeytab();
+                        return null;
+                    });
+                    lastLoginTimestampSeconds = TimeUnit.MILLISECONDS.toSeconds(System.currentTimeMillis());
+                }
+                Path hdfsDirPath = new Path(taskConfig.getDestinationPath());
+                Path hdfsDestinationPath = new Path(hdfsDirPath, UUID.randomUUID().toString());
+                LOGGER.trace("Attempting to create file [{}] by [{}]", hdfsDestinationPath, ugi);
                 ugi.doAs((PrivilegedExceptionAction<Void>) () -> {
-                    ugi.checkTGTAndReloginFromKeytab();
+                    FileSystem fileSystem = FileSystem.get(configuration);
+                    try (FSDataOutputStream outputStream = fileSystem.create(hdfsDestinationPath, true)) {
+                        outputStream.writeChars(String.format("Written at %s", new Date()));
+                        outputStream.flush();
+                        LOGGER.info("Created file [{}] by [{}]", hdfsDestinationPath, ugi);
+                    }
                     return null;
                 });
-                LOGGER.info("Attempting to list status of [{}] with [{}]", hdfsDir, ugi);
-                FileSystem fileSystem = ugi.doAs((PrivilegedExceptionAction<FileSystem>) () -> FileSystem.get(configuration));
-                FileStatus status = ugi.doAs((PrivilegedExceptionAction<FileStatus>) () -> fileSystem.getFileStatus(new Path(hdfsDir)));
-                LOGGER.info("Status retrieved for path [{}] by [{}]: {}", hdfsDir, ugi, status);
             } catch (InterruptedException e) {
                 running = false;
                 Thread.currentThread().interrupt();
